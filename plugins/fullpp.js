5d48b4b14b7819c18a6106ed6f7868b8d3c901ea
PAGAL78 <pairnew07@gmail.com>
2025-05-18 10:35:04 +0500

    Update fullpp.js

--- plugins/fullpp.js
+++ plugins/fullpp.js
@@ -1,5 +1,6 @@
+const config = require('../config'); 
 const { cmd } = require('../command'); 
-const { getBuffer } = require('../lib/functions3'); 
+const { getBuffer } = require('../lib/functions'); 
 const Jimp = require('jimp'); 
 
 cmd({ 
